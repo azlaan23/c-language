d79dad7e0aff25b2ab8a9cde5ba5f5505b53ce79
Aasif hamid <102460661+azlaan23@users.noreply.github.com>
2022-11-19 10:43:13 +0530

    Add files via upload

---
+++ specifier.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+ #define PI 3.43 //we can not use semicolum in preprocessor 
+int main  ()
+{
+   /* int a = 8;
+    float b = 7.998;
+    printf ("Hello world ");
+
+    // printf("the value of a is %d\n and the value of b %f\n", a, b);
+
+
+    // using for space in float -23.4 or 20.6 
+    printf(" the value of b %8.4f\n",  b);**/
+
+
+//  Constant ;-
+// const int a =234;
+//  a = 2365 ; connot do this a is a constant
+//PI = 7.5609 ; // connot do this since PI is a constant 
+// printf("%f ", PI);
+return 0;
+
+// Escape Sequence list in c
+//  printf (" tab character \t\t my blackslasgh \a %d ", );
+
+
+
+}
