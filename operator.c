e7acdc4818259eb5f508b095676c70c5d9b3a617
Aasif hamid <102460661+azlaan23@users.noreply.github.com>
2022-11-19 10:42:48 +0530

    Add files via upload

---
+++ operator.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+int main()
+{
+    // int a, b;
+    // a = 4;
+    // b = 2;
+    // printf("a + b = %d\n", a+b);
+    //  printf ( " a - b = %d\n", a-b);
+    //  printf (" a*b = %d\n", a* b);
+    //  printf (" a/b =%d\n  ", a/b);
+
+    //  if we float then
+    // int a;
+    // float  b;
+    // a = 4;
+    // b = 2.54;
+    // printf("a + b = %d\n", a + b);
+    // printf(" a - b = %d\n", a - b);
+    // printf(" a*b = %d\n", a * b);
+    // printf(" a/b =%d\n  ", a / b);
+
+
+    // Rational operator 
+    // int a;
+    // int b;
+    // a = 6;
+    // b = 6;
+    // printf (" a+b = %d", a==b);
+       
+       int a, b;
+       a = 4 ;
+       b = 4;
+    //    printf(" a && b =%d", a&&b );
+    //    && means true or false
+    //    printf (" a || b = %d", a||b );
+    //    || means or
+printf ("a ! b = %d" , !b);
+       
+       
+       
+        return 0;
+}
