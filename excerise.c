0e37062c6a920c79b1d16a607dc3b3fe6136305f
Aasif hamid <102460661+azlaan23@users.noreply.github.com>
2022-11-19 10:44:34 +0530

    Add files via upload

---
+++ excerise.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/* Print multiplcation table of a number of a entered bt the user in pretty form
+
+
+Example:
+Input the number you want multipcation table off :
+6 
+
+ output :
+ 6 * 1 = 6 
+ 6 *2 = 12.
+ .
+ .
+ .
+ .
+ 6*10 = 60
+ 
+
+*/
