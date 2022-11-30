45ae7ee621f2e28a93fecf1015b42d6f96d4a6da
Aasif hamid <102460661+azlaan23@users.noreply.github.com>
2022-11-30 12:02:10 +0530

    Add files via upload

---
+++ execrise _3.c
@@ -0,0 +1,26 @@
+/* take input from the user and ask user to chooose 0 for trinagular star pattern and 1 for reversed trinagular star pattern
+
+
+
+*
+**
+***
+****
+*****
+*******    ---> Trinagular star patteern
+
+******
+*****
+****
+***
+**
+*----- > Reverse trinagular star pattern */
+
+
+
+#include <stdio.h>
+ int main() 
+{
+
+     return 0;
+}
