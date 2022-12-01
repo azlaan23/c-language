ef93ac80807fcdc4c303da70c2bf3e904009d6c4
Aasif hamid <102460661+azlaan23@users.noreply.github.com>
2022-12-01 11:20:26 +0530

    Add files via upload

---
+++ string_function.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+    char s1[] = "harry";
+    char s2[] = "ravi" ;
+    // aavi 
+    char s3 [53];
+printf("the strcmp for s1, s2, returned %d", strcmp(s1,s2));
+
+
+
+
+//  puts(strcat(s1,s2));
+//  printf("the length  of s1 is %d\n", strlen(s1));
+//  printf("the length of s2 is %d\n", strlen(s2));
+
+
+// printf("the reversed string s1 is :");
+
+
+// puts (strrev(s1));
+// strcpy(s3, strcat(s1, s2));
+// puts (s3);
+
+
+
+    return 0;
+}
